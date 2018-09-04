257c2d7040938240614f9a609aebdf6b63498db2
Shakeel rauf <shkeelrauf@gmail.com>
2018-09-04 23:38:12 +0500

    Expands the tabs on first time.

--- app/services/getswift/delivery.rb
+++ app/services/getswift/delivery.rb
@@ -10,15 +10,18 @@ module Getswift
     end
 
     def self.all_bookings(query)
-      @response = Request.where('api/v2/deliveries',query)
+      response = Request.where('api/v2/deliveries',query)
+      response
     end
 
     def self.show_booking(delivery_id)
-      @response = Request.where("api/v2/deliveries/#{delivery_id}")
+      response = Request.where("api/v2/deliveries/#{delivery_id}")
+      response
     end
 
     def self.cancel_booking(query={})
-      @response = Request.where("api/v2/deliveries/cancel",query, "post")
+      response = Request.where("api/v2/deliveries/cancel",query, "post")
+      response
     end
   end
 end
