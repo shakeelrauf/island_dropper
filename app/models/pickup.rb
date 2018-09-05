e5ff7f19c126babf9c196c5e319a84648aa6a2e1
Shakeel rauf <shkeelrauf@gmail.com>
2018-09-05 17:26:11 +0500

    Collapse and expands feature.

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -1,3 +1,18 @@
 class Pickup < ApplicationRecord
   belongs_to :delivery
+
+  # include Form
+  # include CheckField
+  # validate do |pickup|
+  #   pickup.errors.add(:base, 'An pickup must have a first name.') unless pickup.first_name?
+  # end
+  # cattr_accessor :form_steps do
+  #   %w[pickup dropoff_items]
+  # end
+
+  # attr_accessor :form_step
+
+  # with_options if: -> { required_for_step?(:pickup) } do |step|
+  # end
+
 end
