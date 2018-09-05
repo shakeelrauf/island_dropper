bc1c08cd0ce18d622f7e29840217612da87d4a47
Shakeel rauf <shkeelrauf@gmail.com>
2018-09-05 15:27:34 +0500

    URL change for mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'www.island-dropper.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://island-dropper.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
