22f676e6b03f2d4c22810e1560c0f2006b0fe7d8
Shakeel rauf <shkeelrauf@gmail.com>
2018-09-13 00:07:51 +0500

    Geo location validation.

--- app/models/concerns/geolocation.rb
+++ app/models/concerns/geolocation.rb
@@ -2,9 +2,9 @@ module Geolocation
   extend ActiveSupport::Concern
 
   included do
-    # include ActiveModel::Validations
-    # validates_with Validators::GeoLocationValidator
-  acts_as_mappable :default_units => :kms,
+    include ActiveModel::Validations
+    validates_with Validators::GeoLocationValidator
+    acts_as_mappable :default_units => :kms,
                    :default_formula => :sphere,
                    :distance_field_name => :address,
                    :lat_column_name => :latitude,
