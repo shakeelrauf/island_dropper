6f175929f5998ee7a8398ea83872a15cb1a36c3e
Shakeel rauf <shkeelrauf@gmail.com>
2018-09-13 16:33:22 +0500

    Validating address before save api keys added.

--- app/models/concerns/geolocation.rb
+++ app/models/concerns/geolocation.rb
@@ -2,8 +2,8 @@ module Geolocation
   extend ActiveSupport::Concern
 
   included do
-    # include ActiveModel::Validations
-    # validates_with Validators::GeoLocationValidator
+    include ActiveModel::Validations
+    validates_with Validators::GeoLocationValidator
     acts_as_mappable :default_units => :kms,
                    :default_formula => :sphere,
                    :distance_field_name => :address,
