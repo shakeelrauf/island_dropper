d8df72c79e11e8ac63371f7a04c4ef9ec14b8b09
Shakeel Rauf <shkeelrauf@gmail.com>
2018-09-19 15:14:48 +0500

    Showing error messages.

--- app/helpers/delivery_steps_helper.rb
+++ app/helpers/delivery_steps_helper.rb
@@ -9,7 +9,7 @@ module DeliveryStepsHelper
         base_rate, per_km_rate = size_price(size, pricing)
         res = Getswift::Request.find_distance({origin: delivery.pickup.address,destination: d.address,sensor: false, key: ENV['GEOCODE_API_KEY'] })
         if res[0]["error_messag"].present? or res[0]["routes"][0].nil?
-          flash[:error] = "Dropoff or pickup locations are not correct"
+          flash[:error] = res[0]["error_messag"]
           return nil
         end
         kms = res[0]["routes"][0]["legs"][0]["distance"]["text"].to_f
