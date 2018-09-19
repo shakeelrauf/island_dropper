755e4fcfbfa3aaf37abe1e623ae08d711be25b99
Shakeel Rauf <shkeelrauf@gmail.com>
2018-09-19 14:40:14 +0500

    Design updates.

--- app/helpers/form_error.rb
+++ app/helpers/form_error.rb
@@ -2,7 +2,7 @@ module FormError
    def error_message_on(object, method)
      return unless object.respond_to?(:errors) && object.errors.include?(method)
      errors = field_errors(object, method)
-     content_tag(:div, errors, class: "bg-red text-danger pl-md-3 has-warning")
+     content_tag(:div, errors, class: "bg-red text-danger text-right mt-1 has-warning")
    end
 
    private
